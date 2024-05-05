490978090153ab7314ba1950bc4bf574d814d9fd
Al Rubio <alrubiom.web@gmail.com>
2024-05-05 22:43:41 +0200

    Configurar vue.config.js para que permita el host específico de la app y los subdominios de herokuapp.com

--- vue.config.js
+++ vue.config.js
@@ -12,9 +12,8 @@
  * @license AGPL-3.0-or-later <https://www.gnu.org/licenses/agpl-3.0.html>
  */
 
-
-const fs = require('fs');
 /* Configuración para desarrollo local
+const fs = require('fs');
 const { defineConfig } = require('@vue/cli-service');
 
 
@@ -36,17 +35,8 @@ module.exports = defineConfig({
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    allowedHosts: [
-      '.herokuapp.com', // Permite todos los subdominios de herokuapp.com
-      'baku-rental-manager-frontend-fd6687d31d88.herokuapp.com' // Permite el host específico de tu app
-    ],
-    https: process.env.NODE_ENV === 'development' ? {
-      key: fs.readFileSync(process.env.CERT_KEY_FILE),
-      cert: fs.readFileSync(process.env.CERT_FILE),
-    } : undefined
-  }
+  transpileDependencies: true
+
 });
 
 
